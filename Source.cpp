ce2ed7ba847136d3987f53c9baaa1ef19ff83514
Артемий Зюльков <temka1337@gmail.com>
2020-03-16 11:46:36 +0300

    first

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char main_menu_user_value;
+
+	cout << "[1] Dobavit' v o4ered'" << endl;
+	cout << "[2] Izvle4' iz o4eredi" << endl;
+	cout << "[3] Show o4ered'" << endl;
+	cout << "[4] Zadanie 8 varianta" << endl;
+	cout << "[5] Copy of o4eredi" << endl;
+	cout << "[6] Merge original with copy and show" << endl;
+	cout << "[7] Exit" << endl;
+	cout << "Type in here -> " << endl; cin >> main_menu_user_value;
+
+	switch (main_menu_user_value)
+	{
+	case '1':
+
+	default:return 0;
+		break;
+	}
+
+}
+
